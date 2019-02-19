8adf5d671bd7663dba93c348225283d1273905b1
ricoping <47266653+ricoping@users.noreply.github.com>
2019-02-19 23:38:38 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,7 +19,7 @@ class Scraper():
 		def dynamicUrlParser(self):
 			options = Options()
 			options.add_argument('-headless')
-			exe_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "static", "geckodriver.exe"))
+			exe_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "geckodriver.exe"))
 			browser = webdriver.Firefox(executable_path = exe_path, firefox_options=options)
 			browser.get(self.url)
 			source = browser.page_source
@@ -29,7 +29,7 @@ class Scraper():
 		def dynamicUrlParserByTarget(self, target):
 			options = Options()
 			options.add_argument('-headless')
-			exe_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "static", "geckodriver.exe"))
+			exe_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "geckodriver.exe"))
 			browser = webdriver.Firefox(executable_path = exe_path, firefox_options=options)
 			browser.get(self.url)
 			element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "js-stream-item")))
